502d0fd50b03af056d25427dcd3a93418288c7c1
diegoasua <baxuaa@gmail.com>
2023-05-16 16:39:36 +0100

    feat: Progress bar for upload, resumable upload

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { RiScissors2Fill } from 'react-icons/ri';
@@ -13,6 +13,9 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     const [videoUrl, setVideoUrl] = useState('https://www.w3schools.com/html/mov_bbb.mp4');
     const [isExporting, setIsExporting] = useState(false);
+    const [uploadProgress, setUploadProgress] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isUploading, setIsUploading] = useState(false);
 
     const onDrop = async (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
@@ -26,26 +29,43 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
                 // Upload to Firebase Storage
                 const storage = getStorage(app);
-                const storageRef = ref(storage, 'raw-upload/' + file.name);
-                await uploadBytes(storageRef, file).then(() => {
-                    console.log('Uploaded ', file.name);
-                }).catch((error) => {
-                    console.error('Error uploading file: ', error);
-                });
+                const storageRef = ref(storage, 'uploads/' + file.name);
+
+                setIsUploading(true); // Start uploading
+
+                const uploadTask = uploadBytesResumable(storageRef, file);
+
+                uploadTask.on('state_changed',
+                    (snapshot) => {
+                        // Observe state change events such as progress, pause, and resume
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        setUploadProgress(progress);
+                    },
+                    (error) => {
+                        // Handle unsuccessful uploads
+                        console.error('Error uploading file: ', error);
+                    },
+                    () => {
+                        // Handle successful uploads on complete
+                        console.log('Uploaded ', file.name);
+                        setIsUploading(false); // Stop uploading
+                        setUploadProgress(0); // Reset progress
+                    }
+                );
             } else {
                 console.error('Error: File is null');
             }
         }
     };
 
+
     const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
     };
-
     const handleClickStory = () => {
         setIsExporting(false);
     }
-
     const handleClickExport = () => {
         setIsExporting(true);
     }
@@ -85,36 +105,48 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                             />
                         </div>
                     </div>
-
                     <div className="flex items-start justify-center p-4 py-14">
                         <video className="w-full h-auto max-h-full rounded-md shadow-lg" controls>
                             <source src={videoUrl} type="video/mp4" />
                             Your browser does not support the video tag.
                         </video>
                     </div>
-                    <div className={`flex items-center justify-center p-4 py-14 ${isExporting ? '' : 'invisible'}`}>
+                    <div className={`flex items-center justify-center p-4 py-14 ${isExporting || isUploading ? '' : 'invisible'}`}>
                         <nav className="flex flex-col space-y-4">
-                            <button className="px-8 py-4 font-semibold text-indigo-500 bg-white hover:bg-gray-200 rounded-md border border-indigo-500 text-xl">
-                                Export as MP4
-                            </button>
-                            <button className="px-6 py-4 font-semibold text-indigo-500 bg-white hover:bg-gray-200 rounded-md border border-indigo-500 text-xl">
-                                Export to Premiere
-                            </button>
+                            {isUploading
+                                ? (
+                                    <>
+                                        <div className="relative pt-1">
+                                            <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-indigo-200">
+                                                <div style={{ width: `${uploadProgress}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-indigo-500"></div>
+                                            </div>
+                                        </div>
+                                        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-indigo-500"></div>
+                                    </>
+                                )
+                                : (
+                                    <>
+                                        <button className={`px-8 py-4 font-semibold text-indigo-500 bg-white hover:bg-gray-200 rounded-md border border-indigo-500 text-xl ${isExporting ? '' : 'hidden'}`}>
+                                            Export as MP4
+                                        </button>
+                                        <button className={`px-6 py-4 font-semibold text-indigo-500 bg-white hover:bg-gray-200 rounded-md border border-indigo-500 text-xl ${isExporting ? '' : 'hidden'}`}>
+                                            Export to Premiere
+                                        </button>
+                                    </>
+                                )
+                            }
                         </nav>
                     </div>
-                </div>
 
+                </div>
                 <div className="items-center justify-center h-32 flex items-start bg-gray-200 relative" onDrop={onDrop} onDragOver={onDragOver}>
                     <div className="text-center text-4xl font-bold text-gray-500">Drop a video to start editing</div>
-
                     <div className="absolute top-0 left-0 bottom-0 w-8 bg-gray-300 border-r-2 border-t-2 border-black flex items-center justify-top flex-col">
                         <TbPointer className="mt-4 mb-2" />
                         <RiScissors2Fill />
                     </div>
                 </div>
             </main>
-
-
         </div>
     );
 };
